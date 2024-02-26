ec7d2f4e95cc5d3b2faa891b82203056fc6f9212
Kiwon Kim <kiwon@elicer.com>
2024-02-27 01:13:02 +0900

    feat: add delete feature

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,32 +1,49 @@
 const BASE_URL = 'http://localhost:3001/api';
 
 const input = document.querySelector('input');
-const list = document.querySelector('ul');
+const ul = document.querySelector('ul');
+
+const appendTodo = async (id, text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  li.addEventListener('click', async () => {
+    try {
+      await fetch(BASE_URL + '/todos/' + id, {
+        method: 'DELETE',
+      });
+      li.remove();
+    } catch {
+      console.error('error');
+    }
+  });
+  ul.append(li);
+};
+
 const button = document.querySelector('button');
 button.addEventListener('click', async () => {
   try {
-    await fetch(BASE_URL + '/todos', {
+    const res = await fetch(BASE_URL + '/todos', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ text: input.value }),
     });
-
-    list.insertAdjacentHTML('beforeend', `<li>${input.value}</li>`);
+    const data = await res.json();
+    appendTodo(data._id, input.value);
   } catch {
     console.error('error');
   }
 });
 
-(async () => {
+const main = async () => {
   try {
     const res = await fetch(BASE_URL + '/todos');
     const data = await res.json();
-    data.forEach(todo => {
-      list.insertAdjacentHTML('beforeend', `<li>${todo.text}</li>`);
-    });
+    data.forEach(todo => appendTodo(todo._id, todo.text));
   } catch {
     console.error('error');
   }
-})();
+};
+
+main();
